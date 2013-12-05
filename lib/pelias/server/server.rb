ff0a83644b602a517fa26f88f2ed62676dc42af4
Randy Meech <randy.meech@gmail.com>
2013-12-05 16:19:22 -0500

    search endpoint

--- lib/pelias/server/server.rb
+++ lib/pelias/server/server.rb
@@ -4,14 +4,20 @@ require 'pelias'
 class Server < Sinatra::Base
 
   get '/search' do
-    Pelias::Search.search(
-      params[:query],
-      params[:viewbox],
-      params[:center]
-    )['hits']['hits'].to_json
+    results = Pelias::Search.search(params[:query], params[:viewbox],
+      params[:center])['hits']['hits']
+    results = results.map do |result|
+      {
+        name: result['name'],
+        lat: result['center_point'][1],
+        lon: result['center_point'][0],
+        type: result['suggest']['payload']['type']
+      }
+    end
+    results.to_json
   end
 
-  get '/suggest/:query' do
+  get '/suggest' do
     results = Pelias::Search.suggest(params[:query])
     results['suggestions'][0]['options'].to_json
   end
