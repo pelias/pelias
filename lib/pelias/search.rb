4ff5f4cbc03c1cbe21190f3de67ab4c77493cbf3
Randy Meech <randy.meech@gmail.com>
2014-01-08 09:02:18 -0500

    add filter & sort back

--- lib/pelias/search.rb
+++ lib/pelias/search.rb
@@ -33,6 +33,40 @@ module Pelias
           }
         }
       }
+      if viewbox
+        viewbox = viewbox.split(',')
+        query[:filter] = {
+          geo_bounding_box: {
+            center_point: {
+              top_left: {
+                lat: viewbox[1],
+                lon: viewbox[0]
+              },
+              bottom_right: {
+                lat: viewbox[3],
+                lon: viewbox[2]
+              }
+            }
+          }
+        }
+        unless center
+          center_lon = ((viewbox[0].to_f-viewbox[2].to_f)/2)+viewbox[2].to_f
+          center_lat = ((viewbox[1].to_f-viewbox[3].to_f)/2)+viewbox[3].to_f
+          center = "#{center_lon.round(6)},#{center_lat.round(6)}"
+        end
+      end
+      if center
+        center = center.split(',')
+        query[:sort] = [
+          {
+            _geo_distance: {
+              center_point: [center[0], center[1]],
+              order: 'asc',
+              unit: 'km'
+            }
+          }
+        ]
+      end
       ES_CLIENT.search(index: Pelias::INDEX, body: query, size: size)
     end
 
