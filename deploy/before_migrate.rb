746dfef9e0a0aa9a11210b6f59150742db672a3c
Grant Heffernan <heffergm@gmail.com>
2014-02-06 11:31:44 -0500

    before chef hook

---
+++ deploy/before_migrate.rb
@@ -0,0 +1,9 @@
+Chef::Log.info("Running deploy/before_migrate.rb...")
+
+cfgs = %w(elasticsearch.yml postgres.yml redis.yml).each do |cfg|
+  Chef::Log.info("Symlinking #{release_path}/config/#{cfg} to /etc/mapzen_pelias/#{cfg}")
+  link "#{release_path}/config/#{cfg}" do
+    to "/etc/mapzen_pelias/#{cfg}"
+  end
+end
+
