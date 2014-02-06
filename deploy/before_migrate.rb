0989498b6d541f61c03868b1182d622a3dc5e202
Grant Heffernan <heffergm@gmail.com>
2014-02-06 12:57:10 -0500

    set attributes for cfg location in deploy hooks

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,9 +1,9 @@
 Chef::Log.info("Running deploy/before_migrate.rb...")
 
 node[:mapzen_pelias][:custom_cfgs].each do |cfg|
-  Chef::Log.info("Symlinking #{release_path}/config/#{cfg} to /etc/mapzen_pelias/#{cfg}")
+  Chef::Log.info("Symlinking #{release_path}/config/#{cfg} to #{node[:mapzen_pelias][:cfg_dir]}/#{cfg}")
   link "#{release_path}/config/#{cfg}" do
-    to "/etc/mapzen_pelias/#{cfg}"
+    to "#{node[:mapzen_pelias][:cfg_dir]}/#{cfg}"
   end
 end
 
