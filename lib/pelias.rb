9d4b4e06ff8827f551d948dab0e0e4cdee9cf3e7
John Crepezzi <john.crepezzi@gmail.com>
2014-02-24 16:25:37 -0500

    Remove old geoname classes

--- lib/pelias.rb
+++ lib/pelias.rb
@@ -8,19 +8,12 @@ module Pelias
   autoload :VERSION, 'pelias/version'
 
   autoload :LocationSet, 'pelias/location_set'
+  autoload :Search, 'pelias/search'
 
   autoload :Address, 'pelias/address'
-  autoload :Admin2, 'pelias/admin2'
   autoload :Base, 'pelias/base'
-  autoload :Geoname, 'pelias/geoname'
-  autoload :LocalAdmin, 'pelias/local_admin'
-  autoload :Locality, 'pelias/locality'
-  autoload :Neighborhood, 'pelias/neighborhood'
   autoload :Poi, 'pelias/poi'
   autoload :Street, 'pelias/street'
-
-  autoload :Location, 'pelias/location'
-  autoload :Search, 'pelias/search'
 
   # Load configurations
   Dir.glob('config/initializers/**/*.rb').each { |f| load(f) }
