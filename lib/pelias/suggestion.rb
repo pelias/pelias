ae12dc7e59ff888aa8cb9680db453fa64d8903cc
John Crepezzi <john.crepezzi@gmail.com>
2014-02-25 13:31:07 -0500

    More suggestions

--- lib/pelias/suggestion.rb
+++ lib/pelias/suggestion.rb
@@ -20,30 +20,45 @@ module Pelias
       {
         input:  [e.admin1_abbr, e.admin1_name].compact.map { |v| "#{e.name} #{v}" },
         output: [e.name, e.admin1_abbr || e.admin1_name].compact.join(', '),
-        weight: entry['population'] / 100_000
+        weight: e.population.to_i / 100_000
       }
     end
 
     def rebuild_suggestions_for_local_admin(entry)
       e = Hashie::Mash.new(entry)
+      inputs = []
+      inputs.concat [e.admin1_abbr, e.admin1_name].compact.map { |v| "#{e.name} #{v}" }
+      inputs.concat [e.locality_name, e.admin2_name].compact.map { |v| [e.name, v, e.admin1_abbr || e.admin1_name].join(' ') }
       {
-        input: nil, # TODO
+        input: inputs,
         output: [e.name, e.admin1_abr || e.admin1_name].compact.join(', '),
-        weight: (entry['population'] / 100_000) * 12
+        weight: (e.population.to_i / 100_000) * 12
       }
     end
 
     def rebuild_suggestions_for_locality(entry)
       e = Hashie::Mash.new(entry)
+      inputs = []
+      inputs.concat [e.admin1_abbr, e.admin1_name].compact.map { |v| "#{e.name} #{v}" }
+      inputs.concat [e.local_admin_name, e.admin2_name].compact.map { |v| [e.name, v, e.admin1_abbr || e.admin1_name].join(' ') }
       {
-        input: nil, # TODO
+        input: inputs,
         output: [e.name, e.admin1_abbr || e.admin1_name].compact.join(', '),
-        weight: (entry.population / 100_000) * 12
+        weight: (e.population.to_i / 100_000) * 12
       }
     end
 
     def rebuild_suggestions_for_neighborhood(entry)
-      # TODO
+      e = Hashie::Mash.new(entry)
+      adn = e.locality_name || e.local_admin_name || e.admin2_name
+      inputs = []
+      inputs.concat [e.admin1_abbr, e.admin1_name].compact.map { |v| "#{e.name} #{v}" }
+      inputs.concat [e.local_admin_name, e.locality_name, e.admin2_name].compact.map { |v| [e.name, v, e.admin1_abbr || e.admin1_name].join(' ') }
+      {
+        input: inputs,
+        output: [e.name, adn, e.admin1_abbr, e.admin1_name].compact.join(', '),
+        weight: adn ? 10 : 0
+      }
     end
 
     def rebuild_suggestions_for_address(entry)
