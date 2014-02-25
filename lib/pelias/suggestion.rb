eda93eeaff228042f13d153960eec639b7d51d14
John Crepezzi <john.crepezzi@gmail.com>
2014-02-25 11:53:09 -0500

    suggestion

--- lib/pelias/suggestion.rb
+++ lib/pelias/suggestion.rb
@@ -20,8 +20,42 @@ module Pelias
       {
         input:  [e.admin1_abbr, e.admin1_name].compact.map { |v| "#{e.name} #{v}" },
         output: [e.name, e.admin1_abbr || e.admin1_name].compact.join(', '),
-        weight: entry['population'].to_i / 100_000
+        weight: entry['population'] / 100_000
       }
+    end
+
+    def rebuild_suggestions_for_local_admin(entry)
+      e = Hashie::Mash.new(entry)
+      {
+        input: nil, # TODO
+        output: [e.name, e.admin1_abr || e.admin1_name].compact.join(', '),
+        weight: (entry['population'] / 100_000) * 12
+      }
+    end
+
+    def rebuild_suggestions_for_locality(entry)
+      e = Hashie::Mash.new(entry)
+      {
+        input: nil, # TODO
+        output: [e.name, e.admin1_abbr || e.admin1_name].compact.join(', '),
+        weight: (entry.population / 100_000) * 12
+      }
+    end
+
+    def rebuild_suggestions_for_neighborhood(entry)
+      # TODO
+    end
+
+    def rebuild_suggestions_for_address(entry)
+      # TODO
+    end
+
+    def rebuild_suggestions_for_street(entry)
+      # TODO
+    end
+
+    def rebuild_suggestions_for_poi(entry)
+      # TODO
     end
 
   end
