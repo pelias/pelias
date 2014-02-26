80fa634c493383adeb290ebc08172653efdc1c00
John Crepezzi <john.crepezzi@gmail.com>
2014-02-26 11:52:25 -0500

    Allow no encompassing shapes

--- lib/pelias/search.rb
+++ lib/pelias/search.rb
@@ -152,7 +152,7 @@ module Pelias
           }
         }
       })
-      results['hits']['hits']
+      results['hits'] ? results['hits']['hits'] : []
     end
 
     # Return a single shape, or nil
