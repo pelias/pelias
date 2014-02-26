ecdc39baae4fa6d64d237bcab9ddb4249fa91d77
John Crepezzi <john.crepezzi@gmail.com>
2014-02-26 12:40:49 -0500

    Don't do nothing

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -3,15 +3,16 @@ require 'elasticsearch'
 
 # elasticsearch configuration
 es_config = YAML::load(File.open('config/elasticsearch.yml'))
-transport = Elasticsearch::Transport::Transport::HTTP::Faraday.new(hosts: es_config['hosts']) do |c|
-  c.options.timeout = es_config['timeout'] || 5
-  c.options.open_timeout = es_config['open_timeout'] || 2
-end
 
 # put together an elasticsearch client
 Pelias::INDEX = es_config['index']
 Pelias::ES_CLIENT = Elasticsearch::Client.new(
-  transport: transport,
   reload_on_failure: true,
-  randomize_hosts: true
+  randomize_hosts: true,
+  transport_options: {
+    request: {
+      open_timeout: es_config['open_timeout'] || 2,
+      timeout: es_config['timeout'] || 5
+    }
+  }
 )
