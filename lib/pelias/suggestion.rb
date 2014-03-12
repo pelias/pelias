63f1554e150d2f6139c2575f68f8b9db5ef75a81
John Crepezzi <john.crepezzi@gmail.com>
2014-03-12 11:41:14 -0400

    Move POI

--- lib/pelias/suggestion.rb
+++ lib/pelias/suggestion.rb
@@ -82,7 +82,12 @@ module Pelias
     end
 
     def rebuild_suggestions_for_poi(e)
-      # TODO
+      inputs = [e.local_admin_name, e.locality_name, e.neighborhood_name, e.admin2_name].compact.map { |v| [e.name, e.address_name, v].compact.join(' ') }
+      {
+        input: inputs,
+        output: [name, e.address_name, e.local_admin_name || e.locality_name, e.admin1_abbr || e.admin1_name].compact.join(', '),
+        weight: e.locality_name || e.local_admin_name ? 6 : 0
+      }
     end
 
   end
