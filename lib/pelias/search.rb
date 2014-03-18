a3976b81d341c3eb8331bf2deca67611fe102c6f
John Crepezzi <john.crepezzi@gmail.com>
2014-03-18 14:50:16 -0400

    Search on admin1_abbr

--- lib/pelias/search.rb
+++ lib/pelias/search.rb
@@ -4,7 +4,7 @@ module Pelias
 
     extend self
 
-    FIELD_NAMES = ['name^3'].
+    FIELD_NAMES = ['name^3', 'admin1_abbr'].
       concat(QuattroIndexer::SHAPE_ORDER.flat_map { |f| ["#{f}_name", "#{f}_alternate_names"] })
 
     def search(term, viewbox = nil, center = nil, size = 10)
