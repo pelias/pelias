3d5a46bdc236c3b216ed80b14680c4dee3055250
John Crepezzi <john.crepezzi@gmail.com>
2014-04-02 09:53:25 -0400

    Remove deploy folder

--- deploy/before_migrate.rb
+++
@@ -1,9 +0,0 @@
-Chef::Log.info("Running deploy/before_migrate.rb...")
-
-node[:mapzen_pelias][:custom_cfgs].each do |cfg|
-  Chef::Log.info("Symlinking #{release_path}/config/#{cfg} to #{node[:mapzen_pelias][:cfg_dir]}/#{cfg}")
-  link "#{release_path}/config/#{cfg}" do
-    to "#{node[:mapzen_pelias][:cfg_dir]}/#{cfg}"
-  end
-end
-
