32d57e763564d08e653f2619d8cff47ac57059b3
John Crepezzi <john.crepezzi@gmail.com>
2014-04-08 13:46:37 -0400

    Input tests

--- spec/examples/suggestion_spec.rb
+++ spec/examples/suggestion_spec.rb
@@ -9,10 +9,6 @@ describe Pelias::Suggestion do
       let(:data) { { name: 'name' } }
       let(:suggestion) { Pelias::Suggestion.rebuild_suggestions_for_admin0(Hashie::Mash.new(data)) }
 
-      it 'should use the name as an input' do
-        suggestion[:input].should include data[:name]
-      end
-
       it 'should use the name as the output' do
         suggestion[:output].should == data[:name]
       end
@@ -32,10 +28,6 @@ describe Pelias::Suggestion do
       let(:name) { 'hello' }
       let(:data) { { name: name } }
       let(:suggestion) { Pelias::Suggestion.rebuild_suggestions_for_admin1(Hashie::Mash.new(data)) }
-
-      it 'should use the name as an input' do
-        suggestion[:input].should include name
-      end
 
       it 'should use the name as the output' do
         suggestion[:output].should == name
@@ -67,14 +59,6 @@ describe Pelias::Suggestion do
 
       let(:data) { { name: 'name', population: 100_000_000, admin1_name: 'new york', admin1_abbr: 'ny' } }
 
-      it 'should use the admin1_name as an input' do
-        suggestion[:input].should include 'new york'
-      end
-
-      it 'should use the admin1_abbr as an input' do
-        suggestion[:input].should include 'ny'
-      end
-
       it 'should use the name as the output with preference to admin1_abbr' do
         suggestion[:output].should == 'name, ny'
       end
@@ -94,18 +78,6 @@ describe Pelias::Suggestion do
     context 'with a basic local_admin' do
 
       let(:data) { { name: 'name', admin1_abbr: 'a1', admin1_name: 'admin1', locality_name: 'locality', admin2_name: 'admin2', population: 100_000 } }
-
-      it 'should use the name as an input' do
-        suggestion[:input].should include 'name'
-      end
-
-      it 'should use the admin1_abbr as an input' do
-        suggestion[:input].should include 'a1'
-      end
-
-      it 'should use the locality' do
-        suggestion[:input].should include 'locality'
-      end
 
       it 'should construct a friendly output' do
         suggestion[:output].should == 'name, a1'
