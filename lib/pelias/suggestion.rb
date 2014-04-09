e9dc811826dbf7631041c98f900ac89de546cfbc
John Crepezzi <john.crepezzi@gmail.com>
2014-04-09 17:32:44 -0400

    Revert "Update suggestion.rb"
    This reverts commit 2271e8fd3d3889bcbb1798f11c2e3691a8a0837b.

--- lib/pelias/suggestion.rb
+++ lib/pelias/suggestion.rb
@@ -12,6 +12,7 @@ module Pelias
 
     def rebuild_suggestions_for_admin1(e)
       {
+        input: [e.name],
         output: e.name,
         weight: 1
       }
@@ -20,20 +21,25 @@ module Pelias
     def rebuild_suggestions_for_admin2(e)
       boost = e.population.to_i / 100_000
       {
+        input:  [e.name, e.admin1_abbr, e.admin1_name],
         output: [e.name, e.admin1_abbr || e.admin1_name].compact.join(', '),
         weight: boost < 1 ? 1 : boost
       }
     end
 
     def rebuild_suggestions_for_local_admin(e)
+      inputs = [e.name, e.admin1_abbr, e.admin1_name, e.locality_name, e.admin2_name]
       {
+        input: inputs,
         output: [e.name, e.admin1_abbr || e.admin1_name].compact.join(', '),
         weight: (e.population.to_i / 100_000) + 12
       }
     end
 
     def rebuild_suggestions_for_locality(e)
+      inputs = [e.name, e.admin1_abbr, e.admin1_name, e.local_admin_name, e.admin2_name]
       {
+        input: inputs,
         output: [e.name, e.admin1_abbr || e.admin1_name].compact.join(', '),
         weight: (e.population.to_i / 100_000) + 12
       }
@@ -41,7 +47,9 @@ module Pelias
 
     def rebuild_suggestions_for_neighborhood(e)
       adn = e.locality_name || e.local_admin_name || e.admin2_name
+      inputs = [e.name, e.admin1_abbr, e.admin1_name, e.locality_name, e.local_admin_name, e.admin2_name]
       {
+        input: inputs,
         output: [e.name, adn, e.admin1_abbr || e.admin1_name].compact.join(', '),
         weight: adn ? 10 : 0
       }
@@ -49,7 +57,9 @@ module Pelias
 
     def rebuild_suggestions_for_address(e)
       adn = e.local_admin_name || e.locality_name || e.neighborhood_name || e.admin2_name
+      inputs = [e.name, e.local_admin_name, e.locality_name, e.neighborhood_name, e.admin2_name]
       {
+        input: inputs,
         output: [e.name, adn, e.admin1_abbr || e.admin1_name].compact.join(', '),
         weight: adn ? 10 : 0
       }
@@ -57,14 +67,18 @@ module Pelias
 
     def rebuild_suggestions_for_street(e)
       adn = e.local_admin_name || e.locality_name || e.neighborhood_name || e.admin2_name
+      inputs = [e.name, e.local_admin_name, e.locality_name, e.neighborhood_name, e.admin2_name]
       {
+        input: inputs,
         output: [e.name, adn, e.admin1_abbr || e.admin1_name].join(', '),
         weight: adn ? 8 : 0
       }
     end
 
     def rebuild_suggestions_for_poi(e)
+      inputs = [e.name, e.address_name, a.street_name, e.local_admin_name, e.locality_name, e.neighborhood_name, e.admin2_name, e.admin1_name, e.admin1_abbr]
       {
+        input: inputs,
         output: [e.name, e.address_name, e.local_admin_name || e.locality_name, e.admin1_abbr || e.admin1_name].compact.join(', '),
         weight: e.locality_name || e.local_admin_name ? 6 : 0
       }
